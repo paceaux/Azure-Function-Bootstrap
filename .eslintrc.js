e032bbd1b73d103c501caf7ce22e350042f382c1
Frank Taylor <Frank.Taylor@exlrt.com>
2019-12-08 20:46:07 -0600

    sets indentation rule to match most of repo. adds rules useful for azure

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,18 +4,35 @@ module.exports = {
         "commonjs": true,
         "es6": true
     },
-    "extends": "eslint:recommended",
+    "extends": "airbnb-base/legacy",
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly",
         "context": "readonly",
         "process": "readonly",
-        "console": "readonly"
+        "console": "readonly",
     },
     "parserOptions": {
-        "ecmaVersion": 2018
+        "ecmaVersion": 2019
     },
-    "rules": {
-        "no-console": "warn"
-    }
+	"rules": {
+        'no-tabs': 0,
+        'indent': ['error', 'tab'],
+		'no-console': 'warn',
+		'comma-dangle': [
+			'warn',
+			'never'
+        ],
+		'linebreak-style': [
+			'warn',
+			'unix'
+		],
+		'semi': [
+			'error',
+			'always',
+			{ 'omitLastInOneLineBlock': true}
+		],
+		"block-scoped-var": "error",
+		"consistent-return": "error",
+	}
 };
