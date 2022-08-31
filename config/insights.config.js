3ccd576004f2d0f2d90a39f904f6c0326269c6b5
Frank M. Taylor <frtaylor@redhat.com>
2022-08-31 12:14:00 -0500

    updates insights and gets unit tests passing

--- config/insights.config.js
+++ config/insights.config.js
@@ -1,6 +1,13 @@
 const instrumentationKey = process.env.APPINSIGHTS_INSTRUMENTATIONKEY;
+const localSettings = require('../local.settings.json').Values;
 
+/*
+  when an azure function is running, local.settings.json.values is added to process.env
+  However, if you run an azure function locally, and THEN run the unit tests,
+    the unit tests will fail because the appinsights client wasn't instantiated with a valid key
+    For some reason, this gets unit tests to pass
+*/
 module.exports = {
-	instrumentationKey: instrumentationKey || '', // ADD A DEFAULT INSTRUMENTATION KEY IN CASE ONE ISN'T IN LOCAL ENVIRONMENT
+	instrumentationKey: instrumentationKey || localSettings.APPINSIGHTS_INSTRUMENTATIONKEY || '', // ADD A DEFAULT INSTRUMENTATION KEY IN CASE ONE ISN'T IN LOCAL ENVIRONMENT
 	samplingPercentage: 33
 };
