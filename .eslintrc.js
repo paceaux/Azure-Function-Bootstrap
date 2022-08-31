997b9526d4db3f68539ee4631ec9cb60185c7dae
Frank M. Taylor <frtaylor@redhat.com>
2022-08-31 09:39:55 -0500

    sets linter to latest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,42 @@
 module.exports = {
-    "ignorePatterns": [
-        "node_modules",
-    ],
-    "env": {
-        "browser": false,
-        "commonjs": true,
-        "es2022": true,
-        "jest": true,
-    },
-    "extends": "airbnb-base",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly",
-        "context": "readonly",
-        "process": "readonly",
-        "console": "readonly",
-    },
-    "parserOptions": {
-        "ecmaVersion": 'latest'
-    },
-	"rules": {
-        'no-tabs': 0,
-        'indent': ['error', 'tab'],
+	ignorePatterns: [
+		'node_modules'
+	],
+	env: {
+		browser: false,
+		commonjs: true,
+		es2022: true,
+		jest: true
+	},
+	extends: 'airbnb-base',
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+		context: 'readonly',
+		process: 'readonly',
+		console: 'readonly'
+	},
+	parserOptions: {
+		ecmaVersion: 'latest'
+	},
+	rules: {
+		'no-tabs': 0,
+		indent: ['error', 'tab'],
 		'no-console': 'warn',
 		'comma-dangle': [
 			'warn',
 			'never'
-        ],
+		],
 		'linebreak-style': [
 			'warn',
 			'unix'
 		],
-		'semi': [
+		semi: [
 			'error',
 			'always',
-			{ 'omitLastInOneLineBlock': true}
+			{ omitLastInOneLineBlock: true }
 		],
-		"block-scoped-var": "error",
-		"consistent-return": "error",
+		'block-scoped-var': 'error',
+		'consistent-return': 'error'
 	}
 };
